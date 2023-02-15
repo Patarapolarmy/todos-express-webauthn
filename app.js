a323391c35cf03fc83dc9e7c2bb6cfc3698785b0
Patarapol <84073137+Patarapolarmy@users.noreply.github.com>
2023-02-15 22:39:51 +0700

    test

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var session = require('express-session');
 var csrf = require('csurf');
 var passport = require('passport');
 var logger = require('morgan');
+var cors = require('cors');
 
 // pass the session to the connect sqlite3 module
 // allowing it to inherit from session.Store
@@ -23,6 +24,7 @@ app.set('view engine', 'ejs');
 
 app.locals.pluralize = require('pluralize');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
